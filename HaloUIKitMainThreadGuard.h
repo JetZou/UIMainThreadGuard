55eab201eeb22f1dbee71431cb91e1945b85cedb
Jet <jie.zou@yolu-inc.com>
2015-02-08 21:11:13 +0800

    add file

---
+++ HaloUIKitMainThreadGuard.h
@@ -0,0 +1,14 @@
+//
+//  PSPDFUIKitMainThreadGuard.h
+//  YoluAddressBook
+//
+//  Created by Jet on 13-12-15.
+//  Copyright (c) 2013年 Yolu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HaloUIKitMainThreadGuard : NSObject
+void HaloUIKitMainThreadGuardFunc(void);
+
+@end
